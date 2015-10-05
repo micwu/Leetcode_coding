21374daabe8debc7ffce3a4c9ee57809e3341638
mereyct <micwu@users.noreply.github.com>
2015-10-05 18:06:42 +0800

    Create 007Reverse Integer.c

---
+++ 007Reverse Integer.c
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int reverse(int x) {
+        long num = 0;
+        while(x)
+        {
+            num = num*10 + x % 10;
+            x = x / 10;
+        }
+        if(num > 2147483647 || num < -2147483648) return 0;
+        
+        return num;
+    }
+};
+//easy
